71efc93580484749bcbb29dd302f48edc90a08e7
anuraag <anuraag.varanasi@gmail.com>
2022-01-09 17:06:19 +1000

    Multi-user support

--- rat.py
+++ rat.py
@@ -8,24 +8,24 @@ from messages import Messages
 from pretty import pretty_print
 from user_places import UserPlaces
 
-USER = users.init_users()[0]
 MESSAGES = Messages()
 
 def main(): 
-    try:
-        places = get_json()
-        user_places = UserPlaces(places, USER)
-        
-        for x in range(5):
-            distance, place = user_places.sorted_places[x]
-            pretty_print(distance, place)
-        print()
+    places = get_json()
 
-        MESSAGES.send_text(USER, user_places.within_range)
+    for user in users.init_users():
+        try:
+            user_places = UserPlaces(places, user)
+            
+            for x in range(5):
+                distance, place = user_places.sorted_places[x]
+                pretty_print(distance, place)
+            print()
 
-    except Exception as err:
-        print(err)
-        print("Something went wrong. Swalloing issue, will retry in 5 seconds")
+            MESSAGES.send_text(user, user_places.within_range)
+        except Exception as err:
+            print(err)
+            print("Something went wrong. Swalloing issue, will retry in 5 seconds")
 
 def get_json():
         response = requests.get('https://sparkling-voice-bdd0.pipelabs-au.workers.dev/')
